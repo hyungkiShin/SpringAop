fef1c1bc974709aae08f49a04958c1974f984a3d
hyungkiShin <tkaqkeldk@nate.com>
2022-05-04 23:39:34 +0900

    aop 실전예제 추가

---
+++ src/test/java/hello/aop/exam/EnumTest.java
@@ -0,0 +1,35 @@
+package hello.aop.exam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(TraceAspect1.class)
+@SpringBootTest
+public class EnumTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Autowired
+    TestService testService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i ={}", i);
+            examService.request("data" + i);
+        }
+    }
+
+    @Test
+    void test1() {
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i ={}", i);
+            testService.request("data" + i);
+        }
+    }
+}
