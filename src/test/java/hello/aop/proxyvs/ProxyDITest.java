a4af806f417a603989a006eaf7c4db3d2d404dc5
hyungkiShin <tkaqkeldk@nate.com>
2022-05-05 18:34:29 +0900

    프록시 기술의 한계

--- src/test/java/hello/aop/proxyvs/ProxyDITest.java
+++ src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -29,4 +29,8 @@ public class ProxyDITest {
         log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
         memberServiceImpl.hello("hello");
     }
+
+    /**
+     * JDK 동적 프록시는 대상 클래스 타입으로 주입할 때 문제가 있고, CGLIB 는 대상 클래스에 기본 생성자 필수, 생성자 2번 호출 문제가 있다.
+     */
 }
