8610280d33ff3642f2c3e0d44d1880158c8baac0
hyungkiShin <tkaqkeldk@nate.com>
2022-05-05 18:25:05 +0900

    프록시 기술의 한계
    의존관계 주입

---
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,51 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        /**
+         * MemberServiceImpl 은 구체클래스도 있고, 인터페이스도 있다.
+         */
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); // JDK 동적 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        // JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패, ClassCastException 예외 발생
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        /**
+         * MemberServiceImpl 타입을 기반으로 CGLIB 프록시를 생성했다.
+         * MemberServiceImpl 타입은 MemberService 인터페이스를 구현했다.
+         * CGLIB 는 구체 클래스를 기반으로 프록시를 생성한다.
+         * 따라서 CGLIB 는 memberServiceProxy 가 바로 CGLIB Proxy 이다.
+         */
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // CGLIB 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("memberServiceProxy ={} ", memberServiceProxy.getClass());
+
+        // CGLIB 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
+}
